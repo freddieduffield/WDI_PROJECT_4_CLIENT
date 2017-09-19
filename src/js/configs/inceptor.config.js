adb2f737aef0e91d7698e1ba7931b91fc76db096
FrederickFoxxx <fwhduffield@gmail.com>
2017-09-19 10:34:55 +0100

    rewrote and renamed interceptor

--- src/js/configs/inceptor.config.js
+++
@@ -1,8 +0,0 @@
-angular
-.module('ldnFabric')
-.config(Interceptor);
-
-Interceptor.inject = ['$httpProvider', 'AuthInterceptor'];
-function Interceptor($httpProvider) {
-  return $httpProvider.interceptors.push('AuthInterceptor');
-}
